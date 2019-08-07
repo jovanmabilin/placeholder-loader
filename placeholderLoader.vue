c5aabf540d01e452eda30d1ae65735165b291201
Jovan Mabilin <mabilinjovan2@gmail.com>
2019-08-07 18:06:16 +0800

    Placeholder Loader
    Fully function placeholder loader with existing templates

---
+++ placeholderLoader.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="m-0 p-0">
+        <template v-if="isLoading">
+            <component :is="template" :row="row" :column="column"></component>    
+        </template>
+        <template v-else>
+            <slot></slot>
+        </template>
+    </div>
+</template>
+
+<script>
+    /**
+     * CRM Loader
+     * 
+     * Props: template, row, column
+     * Templates:
+     *          - imgBodyVertical
+     *          - imgBodyHorizontal
+     *          - list
+     *          - bulletList
+     */
+    import list from './templates/list';
+    import bulletList from './templates/bulletList';
+    import imgBodyHorizontal from './templates/imgBodyHorizontal';
+    import imgBodyVertical from './templates/imgBodyVertical';
+    export default {
+        props: {
+            template: {
+                type: String,
+                default: 'list',
+            },
+            row: {
+                type: Number,
+                default: 5,
+            },
+            column: {
+                type: Number,
+                default: 1
+            },
+            isLoading: {
+                type: Boolean,
+                default: false,
+            }
+        },
+        components: {
+            list,
+            bulletList,
+            imgBodyHorizontal,
+            imgBodyVertical,
+        },
+        data() {
+            return {
+                loading: false,
+                window: window,
+                height: null,
+            }
+        },
+    }
+</script>
---
+++ templates/list.vue
@@ -0,0 +1,68 @@
+<template>
+    <div>
+        <div class="placeholder-content">
+            <b-row>
+                <b-col v-for="indexColumn in column" :key="indexColumn">
+                    <div  class="placeholder-content-item-container" v-for="indexRow in row" :key="indexRow">
+                        <div class="placeholder-content-item" :style="placeholderItemStyle()"></div>
+                    </div>
+                </b-col>
+            </b-row>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            row: {
+                type: Number,
+                default: 10,
+            },
+            column: {
+                type: Number,
+                default: 1
+            }
+        },
+        data() {
+            return { 
+                isLoading: true,
+                randomWidth: [20,30,40,50,60],
+            }
+        },
+        methods: {
+            placeholderItemStyle() {
+                let random = Math.ceil( Math.random() * ( this.randomWidth.length - 1) );
+                let randomWidth = this.randomWidth[random];
+                return {
+                    width: `${randomWidth}%`,
+                    height: '12px',
+                }
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+.placeholder-content {
+  width: 100%;
+  height: inherit;
+  animation: reduce-opacity 2s linear infinite;
+}
+
+.placeholder-content .placeholder-content-item  {
+  border-radius: 50px;
+  margin-bottom: 10px;
+  background-color: #dddddd80;
+}
+
+.placeholder-content-item-container .placeholder-content-item  {
+    display: inline-block;
+}
+
+@keyframes reduce-opacity {
+      0% { -webkit-filter:    opacity(1);}
+      50% { -webkit-filter:   opacity(.1);}
+      100% { -webkit-filter:  opacity(1);}
+}
+</style>
